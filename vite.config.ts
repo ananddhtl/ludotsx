6ee9e9ee1efb2886c66c094200806bf6ebb96d6f
unknown <anddhital@gmail.com>
2024-12-01 21:32:08 +0545

    Deploy Issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import eslintPlugin from 'vite-plugin-eslint'; // Add eslint plugin
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    eslintPlugin({
+      ignoreDuringBuilds: true, // Ignore ESLint errors during builds
+    }),
+  ],
   server: {
     port: 3000,
   },
